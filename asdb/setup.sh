170ccd4bcbeb27e1b02a03f634aea83b8af07095
animeai <6061753+animeai@users.noreply.github.com>
2022-05-24 20:55:46 +0100

    bugs

--- asdb/setup.sh
+++ asdb/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # Enable dongle
-if [ ! -f /etc/modprobe.d/blacklist-rtl2832.conf ]
-then 
-touch /etc/modprobe.d/blacklist-rtl2832.conf
-echo "blacklist rtl2832" >> /etc/modprobe.d/blacklist-rtl2832.conf
-echo "blacklist dvb_usb_rtl28xxu" >> /etc/modprobe.d/blacklist-rtl2832.conf
-echo "blacklist rtl2832_sdr" >> /etc/modprobe.d/blacklist-rtl2832.conf
-fi
+# if [ ! -f /etc/modprobe.d/blacklist-rtl2832.conf ]
+# then 
+# touch /etc/modprobe.d/blacklist-rtl2832.conf
+# echo "blacklist rtl2832" >> /etc/modprobe.d/blacklist-rtl2832.conf
+# echo "blacklist dvb_usb_rtl28xxu" >> /etc/modprobe.d/blacklist-rtl2832.conf
+# echo "blacklist rtl2832_sdr" >> /etc/modprobe.d/blacklist-rtl2832.conf
+# fi
